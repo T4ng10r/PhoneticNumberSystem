c023ee5bb15dff8c6e7a936268d3ae52d46894c0
T4ng10r <tanglor@poczta.onet.pl>
2015-12-26 11:01:49 +0100

    Small fixes

--- Sources/Main/src/Data/CSubstituteSearch.cpp
+++ Sources/Main/src/Data/CSubstituteSearch.cpp
@@ -2,7 +2,7 @@
 #include <Data/CSubstituteSearch.h>
 #include <Data/Settings.h>
 #include <Data/SearchResultTreeNode.h>
-#include <boost/algorithm/string.hpp>	//boost::to_upper_copy
+#include <boost/algorithm/string.hpp>  //boost::to_upper_copy
 #include <boost/foreach.hpp>
 #include <boost/bind.hpp>
 #include <boost/format.hpp>
@@ -10,120 +10,120 @@
 class CSubstituteSearchPrivate
 {
 public:
-	CSubstituteSearchPrivate(CSubstituteSearch * ptrPublic);
-	~CSubstituteSearchPrivate();
-	bool testWord(const std::string &);
-	void buildSearchResultsTree();
+  CSubstituteSearchPrivate(CSubstituteSearch * ptrPublic);
+  ~CSubstituteSearchPrivate();
+  bool testWord(const std::string &);
+  void buildSearchResultsTree();
 
-	void clearSearchResult();
-	void prepareSearchResults();
+  void clearSearchResult();
+  void prepareSearchResults();
 public:
-	CSubstituteSearch *                    m_ptrPublic;
-	boost::shared_ptr<CDictionaryData>     dictionaryWords;
-	std::string			number;
-	WordsList				searchResult;
-	WordsList				searchCandidates;
-	FittingWordsMap					searchResultMap;
-	SharedTreeNodes					searchResultTreeRoot;
-	CSingleSubstituteDigitsConfiguration digits_conf;
+  CSubstituteSearch *                    m_ptrPublic;
+  boost::shared_ptr<CDictionaryData>     dictionaryWords;
+  std::string                            number;
+  WordsList                              searchResult;
+  WordsList                              searchCandidates;
+  FittingWordsMap                        searchResultMap;
+  SharedTreeNodes                        searchResultTreeRoot;
+  CSingleSubstituteDigitsConfiguration   digits_conf;
 };
 
 CSubstituteSearchPrivate::CSubstituteSearchPrivate(CSubstituteSearch * ptrPublic):m_ptrPublic(ptrPublic),
-	searchResultTreeRoot(new SearchResultTreeNode(0))
+  searchResultTreeRoot(new SearchResultTreeNode(0))
 {
 }
 CSubstituteSearchPrivate::~CSubstituteSearchPrivate(){}
 bool CSubstituteSearchPrivate::testWord( const std::string & wordToTest)
 {
-	SuccessWord	result;
-	MatchingPair matchingPair;
-	std::string  coveredDigits;
-	size_t searchStartPos(0);
-	StartingIndex digitIndex(0);
-	std::string word = boost::to_upper_copy(wordToTest);
-	result.bFullCoverage=true;
-	size_t acceptPos;
-	for(;digitIndex<number.size();digitIndex++)
-	{
-		unsigned int digit = number[digitIndex]-'0';
-		//test 
-		OneDigitConsonantsSet::const_iterator iter = digits_conf.digitsConsonantsSetMap.find(digit);
-		acceptPos = word.find_first_of(iter->second.first,searchStartPos);
-		size_t forbPos = word.find_first_of(iter->second.second,searchStartPos);
-		//if both result are end - we reached end of word or no chars in 
-		if (acceptPos == std::string::npos && forbPos == std::string::npos)
-		{
-			digitIndex--;
-			break;
-		}
-		//if forbidden char is before accept char - we have rejection candidate
-		if (acceptPos>forbPos)
-		{
-			result.matchingLetters.clear();
-			coveredDigits.clear();
-			result.bFullCoverage=false;
-			//
-			matchingPair = MatchingPair();
-		}
-		//ok, expect subset char found
-		if (acceptPos<forbPos)
-		{
-			if (result.matchingLetters.empty())
-				matchingPair.first=digitIndex;
-			result.matchingLetters.push_back(word.at(acceptPos));
-			coveredDigits.push_back(number[digitIndex]);
-			searchStartPos = acceptPos+1;
-		}
-	}
-	result.bFullCoverage=(coveredDigits==number);
-	if (word.find_first_of(digits_conf.allConsonants,searchStartPos)==std::string::npos && coveredDigits.size())
-	{
-		result.words.push_back(wordToTest);
-		if (digitIndex>=number.size())
-			digitIndex=number.size()-1;
-		matchingPair.second=digitIndex;
-		result.coveragePairs.push_back(matchingPair);
-		searchResultMap[matchingPair].push_back(result);
-		if (!result.bFullCoverage)
-		{
-			while((acceptPos = number.find(coveredDigits,matchingPair.second+1))!=std::string::npos)
-			{
-				matchingPair.first=acceptPos;
-				matchingPair.second=acceptPos+coveredDigits.size();
-				result.coveragePairs.front()=matchingPair;
-				searchResultMap[matchingPair].push_back(result);
-			}
-		}
-		return true;
-	}
-	return false;
+  SuccessWord  result;
+  MatchingPair matchingPair;
+  std::string  coveredDigits;
+  size_t searchStartPos(0);
+  StartingIndex digitIndex(0);
+  std::string word = boost::to_upper_copy(wordToTest);
+  result.bFullCoverage=true;
+  size_t acceptPos;
+  for(;digitIndex<number.size();digitIndex++)
+  {
+    unsigned int digit = number[digitIndex]-'0';
+    //test 
+    OneDigitConsonantsSet::const_iterator iter = digits_conf.digitsConsonantsSetMap.find(digit);
+    acceptPos = word.find_first_of(iter->second.first,searchStartPos);
+    size_t forbPos = word.find_first_of(iter->second.second,searchStartPos);
+    //if both result are end - we reached end of word or no chars in 
+    if (acceptPos == std::string::npos && forbPos == std::string::npos)
+    {
+      digitIndex--;
+      break;
+    }
+    //if forbidden char is before accept char - we have rejection candidate
+    if (acceptPos>forbPos)
+    {
+      result.matchingLetters.clear();
+      coveredDigits.clear();
+      result.bFullCoverage=false;
+      //
+      matchingPair = MatchingPair();
+    }
+    //ok, expect subset char found
+    if (acceptPos<forbPos)
+    {
+      if (result.matchingLetters.empty())
+        matchingPair.first=digitIndex;
+      result.matchingLetters.push_back(word.at(acceptPos));
+      coveredDigits.push_back(number[digitIndex]);
+      searchStartPos = acceptPos+1;
+    }
+  }
+  result.bFullCoverage=(coveredDigits==number);
+  if (word.find_first_of(digits_conf.allConsonants,searchStartPos)==std::string::npos && coveredDigits.size())
+  {
+    result.words.push_back(wordToTest);
+    if (digitIndex>=number.size())
+      digitIndex=number.size()-1;
+    matchingPair.second=digitIndex;
+    result.coveragePairs.push_back(matchingPair);
+    searchResultMap[matchingPair].push_back(result);
+    if (!result.bFullCoverage)
+    {
+      while((acceptPos = number.find(coveredDigits,matchingPair.second+1))!=std::string::npos)
+      {
+        matchingPair.first=acceptPos;
+        matchingPair.second=acceptPos+coveredDigits.size();
+        result.coveragePairs.front()=matchingPair;
+        searchResultMap[matchingPair].push_back(result);
+      }
+    }
+    return true;
+  }
+  return false;
 }
 void CSubstituteSearchPrivate::clearSearchResult() 
 {
-	searchResult.clear();
-	searchResultTreeRoot->clear();
-	searchResultTreeRoot->iCurrentIndex=0;
+  searchResult.clear();
+  searchResultTreeRoot->clear();
+  searchResultTreeRoot->iCurrentIndex=0;
 }
 void CSubstituteSearchPrivate::buildSearchResultsTree()
 {
-	clearSearchResult();
+  clearSearchResult();
 
-	printLog(eDebug, eInfoLogLevel, "Building tree of search results");
-	BOOST_FOREACH(const FittingWordsMap::value_type & resultItem, searchResultMap)
-	{
-		const WordsList & searchResult = resultItem.second;
-		StartingIndex start=resultItem.first.first;
-		StartingIndex end=resultItem.first.second;
-		std::for_each(searchResult.begin(),searchResult.end(),
-			boost::bind(&SearchResultTreeNode::addNode, searchResultTreeRoot, start,end, _1));
-	}
+  printLog(eDebug, eInfoLogLevel, "Building tree of search results");
+  BOOST_FOREACH(const FittingWordsMap::value_type & resultItem, searchResultMap)
+  {
+    const WordsList & searchResult = resultItem.second;
+    StartingIndex start=resultItem.first.first;
+    StartingIndex end=resultItem.first.second;
+    std::for_each(searchResult.begin(),searchResult.end(),
+      boost::bind(&SearchResultTreeNode::addNode, searchResultTreeRoot, start,end, _1));
+  }
 }
 void CSubstituteSearchPrivate::prepareSearchResults()
 {
-	printLog(eDebug, eInfoLogLevel, "Building tree of search results");
-	buildSearchResultsTree();
-	printLog(eDebug, eInfoLogLevel, "Gathering processed search results");
-	//searchResult = searchResultTreeRoot->parseDFS(number.length());
+  printLog(eDebug, eInfoLogLevel, "Building tree of search results");
+  buildSearchResultsTree();
+  printLog(eDebug, eInfoLogLevel, "Gathering processed search results");
+  //searchResult = searchResultTreeRoot->parseDFS(number.length());
 }
 //////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////
@@ -133,52 +133,52 @@ CSubstituteSearch::CSubstituteSearch():privPart(new CSubstituteSearchPrivate(thi
 CSubstituteSearch::~CSubstituteSearch(void){}
 void CSubstituteSearch::setSubstituteDigitsConfiguration(CSingleSubstituteDigitsConfiguration conf)
 {
-	privPart->digits_conf = conf;
+  privPart->digits_conf = conf;
 }
 void CSubstituteSearch::setDictionaryWords(boost::shared_ptr<CDictionaryData> dictionaryWords)
 {
-	privPart->searchResultMap.clear();
-	privPart->dictionaryWords = dictionaryWords;
+  privPart->searchResultMap.clear();
+  privPart->dictionaryWords = dictionaryWords;
 }
 void CSubstituteSearch::startSearchForNumber(const std::string & number)
 {
-	printLog(eDebug, eInfoLogLevel, str(boost::format("Searching substitute for number '%1%' started") % number));
-	privPart->clearSearchResult();
-	privPart->number = number;
-	unsigned int words_count = privPart->dictionaryWords->getWordsCount();
-	unsigned int notifyStepCount = words_count/100;
-	unsigned int notifyStep = 0;
-	Q_EMIT searchProgress(0, words_count);
-	for(unsigned int index=0;index<words_count;index++,notifyStep++)
-	{
-		std::string word = privPart->dictionaryWords->get_word_by_index(index);
-		if (word.size()<2)
-			continue;
-		privPart->testWord(word);
-		if (notifyStep==notifyStepCount)
-		{
-			Q_EMIT searchProgress(index+1,words_count);
-			notifyStep=0;
-		}
-	}
-	Q_EMIT searchProgress(words_count,words_count);
-	privPart->buildSearchResultsTree();
-	Q_EMIT searchFinished(true);
-	printLog(eDebug, eInfoLogLevel, str(boost::format("Searching substitute for number '%1%' finished") % number));
+  printLog(eDebug, eInfoLogLevel, str(boost::format("Searching substitute for number '%1%' started") % number));
+  privPart->clearSearchResult();
+  privPart->number = number;
+  unsigned int words_count = privPart->dictionaryWords->getWordsCount();
+  unsigned int notifyStepCount = words_count/100;
+  unsigned int notifyStep = 0;
+  Q_EMIT searchProgress(0, words_count);
+  for(unsigned int index=0;index<words_count;index++,notifyStep++)
+  {
+    std::string word = privPart->dictionaryWords->get_word_by_index(index);
+    if (word.size()<2)
+      continue;
+    privPart->testWord(word);
+    if (notifyStep==notifyStepCount)
+    {
+      Q_EMIT searchProgress(index+1,words_count);
+      notifyStep=0;
+    }
+  }
+  Q_EMIT searchProgress(words_count,words_count);
+  privPart->buildSearchResultsTree();
+  Q_EMIT searchFinished(true);
+  printLog(eDebug, eInfoLogLevel, str(boost::format("Searching substitute for number '%1%' finished") % number));
 }
 WordsList CSubstituteSearch::getSearchResult(StartingIndex start_index)
 {
-	WordsList searchResult;
-	TreeNodesList nodes_list = privPart->searchResultTreeRoot->find_node(start_index);
+  WordsList searchResult;
+  TreeNodesList nodes_list = privPart->searchResultTreeRoot->find_node(start_index);
 
-	for(SharedTreeNodes tree_node : nodes_list)
-	{
-		for(ChildrenMap::value_type & child : tree_node->children)
-		{
-			std::copy(child.second.words_list().begin(),
-				child.second.words_list().end(),
-				back_inserter(searchResult));
-		}
-	}
-	return searchResult;
+  for(SharedTreeNodes tree_node : nodes_list)
+  {
+    for(ChildrenMap::value_type & child : tree_node->children)
+    {
+      std::copy(child.second.words_list().begin(),
+        child.second.words_list().end(),
+        back_inserter(searchResult));
+    }
+  }
+  return searchResult;
 }
--- Sources/Main/src/Data/CSubstituteSearch.h
+++ Sources/Main/src/Data/CSubstituteSearch.h
@@ -11,21 +11,24 @@
 class CSubstituteSearchPrivate;
 
 class CSubstituteSearch : public QObject
+//execute "!find . \( -name '*.h' -o -name '*.cpp' \) -exec sed 's|\\bCSubstituteSearch\\b|MatchingWordsSearch|g' {} \\;"
+
+                          
 {
      Q_OBJECT
      friend class CSubstituteSearchPrivate;
 public:
-     CSubstituteSearch();
-     ~CSubstituteSearch(void);
-	 void setSubstituteDigitsConfiguration(CSingleSubstituteDigitsConfiguration conf);
-	 void setDictionaryWords(boost::shared_ptr<CDictionaryData>);
-	 void startSearchForNumber(const std::string & number);
-	 //const WordsList & getSearchResult();
-	 WordsList getSearchResult(StartingIndex start_index = 0);
+    CSubstituteSearch();
+    ~CSubstituteSearch(void);
+    void setSubstituteDigitsConfiguration(CSingleSubstituteDigitsConfiguration conf);
+    void setDictionaryWords(boost::shared_ptr<CDictionaryData>);
+    void startSearchForNumber(const std::string & number);
+    //const WordsList & getSearchResult();
+    WordsList getSearchResult(StartingIndex start_index = 0);
 Q_SIGNALS:
-	 void searchProgress(int current, int max);
-	 void searchFinished(bool);
+    void searchProgress(int current, int max);
+    void searchFinished(bool);
 protected:
-     boost::scoped_ptr<CSubstituteSearchPrivate> privPart;
+    boost::scoped_ptr<CSubstituteSearchPrivate> privPart;
 };
 #endif //_CSUBSTITUTESEARCH_INCLUDE_
