56ab7ab707e43cb2306f8b2a02f0ea50d4ff953f
T4ng10r <tanglor@povzta.onet.pl>
2016-01-24 11:17:51 +0100

    testWord tests passes

--- Sources/Main/src/Data/MatchingWordsSearch.cpp
+++ Sources/Main/src/Data/MatchingWordsSearch.cpp
@@ -8,29 +8,29 @@
 #include <boost/format.hpp>
 #include <boost/optional.hpp>
 
-
 class MatchingWordsSearchPrivate
 {
-  struct ConsonantsPos
-  {
-    size_t acceptable;
-    size_t forbiden;
-  };
+    struct ConsonantsPos {
+        size_t acceptable;
+        size_t forbiden;
+    };
+
   public:
     MatchingWordsSearchPrivate(MatchingWordsSearch* ptrPublic);
     ~MatchingWordsSearchPrivate();
     boost::optional<MatchingWord> testWord(const std::string& tested_word, const std::string& searched_number);
     void buildSearchResultsTree();
 
-    void clearSearchResult();
-    void prepareSearchResults();
-    ConsonantsPos getConsonatsPos(const std::string& word, unsigned int digit, size_t start );
-    void setStartDigitIndex(MatchingWord& result,size_t digitIndex);
-    void setEndDigitIndex(MatchingWord& result,size_t digitIndex);
+    void          clearSearchResult();
+    void          prepareSearchResults();
+    ConsonantsPos getConsonatsPos(const std::string& word, unsigned int digit, size_t start);
+    void setStartDigitIndex(MatchingWord& result, size_t digitIndex);
+    void setEndDigitIndex(MatchingWord& result, size_t digitIndex);
+    void addMatchingLetter(MatchingWord& result, char letter);
 
   public:
     MatchingWordsSearch*                 m_ptrPublic;
-    boost::shared_ptr<CDictionaryData>   dictionaryWords;
+    boost::shared_ptr<CDictionaryData>   dictionary;
     std::string                          number;
     WordsList                            searchResult;
     WordsList                            searchCandidates;
@@ -47,25 +47,33 @@ MatchingWordsSearchPrivate::MatchingWordsSearchPrivate(MatchingWordsSearch* ptrP
 
 MatchingWordsSearchPrivate::~MatchingWordsSearchPrivate() {}
 
-MatchingWordsSearchPrivate::ConsonantsPos MatchingWordsSearchPrivate::getConsonatsPos(const std::string& word, unsigned int digit, size_t start )
+MatchingWordsSearchPrivate::ConsonantsPos MatchingWordsSearchPrivate::getConsonatsPos(const std::string& word,
+                                                                                      unsigned int digit, size_t start)
 {
-  ConsonantsPos result;
-  OneDigitConsonantsSet::const_iterator iter      = digits_conf.digitsConsonantsSetMap.find(digit);
-  result.acceptable = word.find_first_of(iter->second.acceptable, start);
-  result.forbiden = word.find_first_of(iter->second.forbiden, start);
-  return result;
+    ConsonantsPos                         result;
+    OneDigitConsonantsSet::const_iterator iter = digits_conf.digitsConsonantsSetMap.find(digit);
+    result.acceptable                          = word.find_first_of(iter->second.acceptable, start);
+    result.forbiden                            = word.find_first_of(iter->second.forbiden, start);
+    return result;
 }
 
-void MatchingWordsSearchPrivate::setStartDigitIndex(MatchingWord& result,size_t digitIndex)
+void MatchingWordsSearchPrivate::setStartDigitIndex(MatchingWord& result, size_t digitIndex)
 {
-  if (result.coveredDigitsIndices.back().startIndex == std::string::npos)
-    result.coveredDigitsIndices.back().startIndex = digitIndex;
+    if (result.coveredDigitsIndices.back().startIndex == std::string::npos)
+        result.coveredDigitsIndices.back().startIndex = digitIndex;
 }
 
-void MatchingWordsSearchPrivate::setEndDigitIndex(MatchingWord& result,size_t digitIndex)
+void MatchingWordsSearchPrivate::setEndDigitIndex(MatchingWord& result, size_t digitIndex)
 {
-  if (result.coveredDigitsIndices.back().startIndex != std::string::npos)
-    result.coveredDigitsIndices.back().endIndex = digitIndex;
+    if (result.coveredDigitsIndices.back().startIndex != std::string::npos) {
+        result.coveredDigitsIndices.back().endIndex = digitIndex;
+    }
+}
+
+void MatchingWordsSearchPrivate::addMatchingLetter(MatchingWord& result, char letter)
+{
+    if (result.coveredDigitsIndices.size() <= 1)
+        result.matchingLetters.push_back(letter);
 }
 
 boost::optional<MatchingWord> MatchingWordsSearchPrivate::testWord(const std::string& tested_word,
@@ -76,81 +84,55 @@ boost::optional<MatchingWord> MatchingWordsSearchPrivate::testWord(const std::st
     std::string   coveredDigits;
     size_t        searchStartPos(0);
     StartingIndex digitIndex(0);
-    std::string   word   = boost::to_upper_copy(tested_word);
-    result.bFullCoverage = true;
+    std::string   word = boost::to_upper_copy(tested_word);
     result.coveredDigitsIndices.push_back(MatchingPair());
-    do
-    {
-      result.matchingLetters.clear();
-      coveredDigits.clear();
-      for (; digitIndex < searched_number.size(); digitIndex++) {
-          unsigned int digit = searched_number[digitIndex] - '0';
-          // test
-          ConsonantsPos pos = getConsonatsPos(word, digit, searchStartPos);
-          // if both result are end - we reached end of word or no chars in
-          if (pos.acceptable == std::string::npos && pos.forbiden == std::string::npos) {
-              if (result.coveredDigitsIndices.back().startIndex == std::string::npos)
-                result.coveredDigitsIndices.pop_back();
-              else
-              {
-                setEndDigitIndex(result,digitIndex-1);
-                result.coveredDigitsIndices.push_back(MatchingPair());
-                searchStartPos=0;
-              }
-              break;
-          }
-          // if forbidden char is before accept char - we have rejection candidate
-          else if (pos.acceptable > pos.forbiden) {
-              setEndDigitIndex(result,digitIndex);
-              result.matchingLetters.clear();
-              coveredDigits.clear();
-              result.bFullCoverage = false;
-          }
-          // ok, expect subset char found
-          else if (pos.acceptable  < pos.forbiden) {
-              //if (result.matchingLetters.empty())
-              //    matchingPair.startIndex = digitIndex;
-              setStartDigitIndex(result, digitIndex);
-              result.matchingLetters.push_back(word.at(pos.acceptable));
-              coveredDigits.push_back(searched_number[digitIndex]);
-              searchStartPos = pos.acceptable + 1;
-          }
-          if (digitIndex+1 == searched_number.size())
-          {
-            //if this is last tested digit - check if in word appear any more of acceptable consontant
-            if (tested_word.find_first_of(digits_conf.allConsonants, searchStartPos) == std::string::npos)
-              result.coveredDigitsIndices.back().endIndex=digitIndex;
-            else
-              result.coveredDigitsIndices.pop_back();
-          }
-      }
-      }
-    while(digitIndex<searched_number.size());
-    result.bFullCoverage = (coveredDigits == searched_number);
-    if (coveredDigits.size()==0)
-      return boost::optional<MatchingWord>();
-
-    /*
-    if (word.find_first_of(digits_conf.allConsonants, searchStartPos) == std::string::npos && coveredDigits.size()) {
-        result.words.push_back(tested_word);
-        if (digitIndex >= searched_number.size())
-            digitIndex      = searched_number.size() - 1;
-        matchingPair.endIndex = digitIndex;
-        result.coveredDigitsIndices.push_back(matchingPair);
-        searchResultMap[matchingPair].push_back(result);
-        if (!result.bFullCoverage) {
-            size_t acceptPos;
-            while ((acceptPos = searched_number.find(coveredDigits, matchingPair.endIndex + 1)) != std::string::npos) {
-                matchingPair.startIndex           = acceptPos;
-                matchingPair.endIndex          = acceptPos + coveredDigits.size();
-                result.coveredDigitsIndices.front() = matchingPair;
-                searchResultMap[matchingPair].push_back(result);
+    do {
+        for (; digitIndex < searched_number.size(); digitIndex++) {
+            unsigned int digit = searched_number[digitIndex] - '0';
+            // test
+            ConsonantsPos pos = getConsonatsPos(word, digit, searchStartPos);
+            // if both result are end - we reached end of word or no chars in
+            if (pos.acceptable == std::string::npos && pos.forbiden == std::string::npos) {
+                if (result.coveredDigitsIndices.back().startIndex == std::string::npos)
+                    result.coveredDigitsIndices.pop_back();
+                else {
+                    setEndDigitIndex(result, digitIndex - 1);
+                    result.coveredDigitsIndices.push_back(MatchingPair());
+                    searchStartPos = 0;
+                    coveredDigits.clear();
+                }
+                break;
+            }
+            // if forbidden char is before accept char - we have rejection candidate
+            else if (pos.acceptable > pos.forbiden) {
+                setEndDigitIndex(result, digitIndex);
+                coveredDigits.clear();
+                result.bFullCoverage = false;
+            }
+            // ok, expect subset char found
+            else if (pos.acceptable < pos.forbiden) {
+                // if (result.matchingLetters.empty())
+                //    matchingPair.startIndex = digitIndex;
+                setStartDigitIndex(result, digitIndex);
+                addMatchingLetter(result, word.at(pos.acceptable));
+                coveredDigits.push_back(searched_number[digitIndex]);
+                searchStartPos = pos.acceptable + 1;
+            }
+            if (digitIndex + 1 == searched_number.size()) {
+                // if this is last tested digit - check if in word appear any more of acceptable consontant
+                if (tested_word.find_first_of(digits_conf.allConsonants, searchStartPos) == std::string::npos)
+                    result.coveredDigitsIndices.back().endIndex = digitIndex;
+                else
+                    result.coveredDigitsIndices.pop_back();
             }
         }
+    } while (digitIndex < searched_number.size());
+    result.bFullCoverage = (coveredDigits == searched_number);
+
+    if (result.coveredDigitsIndices.size() == 0)
+        return boost::optional<MatchingWord>();
+    else
         return boost::optional<MatchingWord>(result);
-    }*/
-    return boost::optional<MatchingWord>(result);
-    //return boost::optional<MatchingWord>();
 }
 
 void MatchingWordsSearchPrivate::clearSearchResult()
@@ -195,10 +177,10 @@ void MatchingWordsSearch::setSubstituteDigitsConfiguration(CSingleSubstituteDigi
     privPart->digits_conf = conf;
 }
 
-void MatchingWordsSearch::setDictionaryWords(boost::shared_ptr<CDictionaryData> dictionaryWords)
+void MatchingWordsSearch::setDictionaryWords(boost::shared_ptr<CDictionaryData> dictionary)
 {
     privPart->searchResultMap.clear();
-    privPart->dictionaryWords = dictionaryWords;
+    privPart->dictionary = dictionary;
 }
 
 void MatchingWordsSearch::startSearchForNumber(const std::string& number)
@@ -206,12 +188,12 @@ void MatchingWordsSearch::startSearchForNumber(const std::string& number)
     printLog(eDebug, eInfoLogLevel, str(boost::format("Searching substitute for number '%1%' started") % number));
     privPart->clearSearchResult();
     privPart->number             = number;
-    unsigned int words_count     = privPart->dictionaryWords->getWordsCount();
+    unsigned int words_count     = privPart->dictionary->getWordsCount();
     unsigned int notifyStepCount = words_count / 100;
     unsigned int notifyStep      = 0;
     Q_EMIT       searchProgress(0, words_count);
     for (unsigned int index = 0; index < words_count; index++, notifyStep++) {
-        std::string word = privPart->dictionaryWords->get_word_by_index(index);
+        std::string word = privPart->dictionary->get_word_by_index(index);
         if (word.size() < 2)
             continue;
         privPart->testWord(word, number);
@@ -222,8 +204,8 @@ void MatchingWordsSearch::startSearchForNumber(const std::string& number)
     }
     Q_EMIT searchProgress(words_count, words_count);
     privPart->buildSearchResultsTree();
-    Q_EMIT searchFinished(true);
     printLog(eDebug, eInfoLogLevel, str(boost::format("Searching substitute for number '%1%' finished") % number));
+    Q_EMIT searchFinished(true);
 }
 
 WordsList MatchingWordsSearch::getSearchResult(StartingIndex start_index)
--- Sources/Main/ut/tests/ut_matching_words_search.cpp
+++ Sources/Main/ut/tests/ut_matching_words_search.cpp
@@ -42,7 +42,7 @@ void ut_matching_words_search::test_MatchingWordSearch_data()
     QTest::newRow("fail_01") << std::string("123") << std::string("NERKA") << false << indices 
                               << std::string("") << false << (StartingIndex)0;
 
-    /*indices.clear();
+    indices.clear();
     indices.push_back(std::make_pair(0,1));
     QTest::newRow("whole_01") << std::string("99") << std::string("BABCIA") << true << indices 
                               << std::string("BB") << true << (StartingIndex)1;
@@ -51,25 +51,24 @@ void ut_matching_words_search::test_MatchingWordSearch_data()
     indices.push_back(std::make_pair(0,2));
     QTest::newRow("whole_02") << std::string("034") << std::string("SMAR") << true << indices 
                               << std::string("SMR") << true << (StartingIndex)1;
-    
-    indices.clear();
-    indices.push_back(std::make_pair(2,5));
-    QTest::newRow("partial_01") << std::string("885421866") << std::string("LERNED") << true << indices 
-                                << std::string("LRND") << false << (StartingIndex)5;
 
     indices.clear();
     indices.push_back(std::make_pair(0,1));
-    QTest::newRow("partial_02") << std::string("123") << std::string("ACETON") << true << indices
+    QTest::newRow("partial_03") << std::string("123") << std::string("ACETON") << true << indices
                                 << std::string("TN") << false << (StartingIndex)0;
-   */
     indices.clear();
     indices.push_back(std::make_pair(0,0));
     QTest::newRow("partial_01") << std::string("9") << std::string("AAP") << true << indices
                                 << std::string("P") << true << (StartingIndex)0;
+
+    indices.clear();
+    indices.push_back(std::make_pair(2,5));
+    QTest::newRow("partial_02") << std::string("885421866") << std::string("LERNED") << true << indices 
+                                << std::string("LRND") << false << (StartingIndex)5;
     indices.clear();
     indices.push_back(std::make_pair(0,0));
     indices.push_back(std::make_pair(1,1));
-    QTest::newRow("partial_03") << std::string("99") << std::string("AAP") << true << indices
+    QTest::newRow("partial_two_times") << std::string("99") << std::string("AAP") << true << indices
                                 << std::string("P") << false << (StartingIndex)0;
 }
 
@@ -91,7 +90,7 @@ void ut_matching_words_search::test_MatchingWordSearch()
     // substituteSearchPrivate->searchResultMap.find(MatchingPair(start_index,end_index));
     // QVERIFY(iter!=substituteSearchPrivate->searchResultMap.end());
     // QCOMPARE(iter->second.size(), (std::size_t)size);
-    QCOMPARE(result->matchingLetters, matching_letters);
+    QCOMPARE(result->matchingLetters.c_str(), matching_letters.c_str());
     QCOMPARE(result->bFullCoverage, full_coverage);
     std::list<MatchingPair>::const_iterator iter = result->coveredDigitsIndices.begin();
     for(int i=0;i<indices.size();i++)
